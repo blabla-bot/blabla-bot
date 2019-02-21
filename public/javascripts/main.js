76be89cbf70c7143dca71ed7c34f0f8202dd03bf
david <david@konecny.eu>
2019-02-21 19:45:56 +0100

    Button disabled

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,13 @@
 const button    = document.getElementById("generate");
+const spinner    = document.getElementById("generate-spinner");
+
 const num       = document.getElementById("paragraphs");
 const dict      = document.getElementById("dict");
 const content   = document.getElementById("content");
 
 const queryForData = (e) => {
+    button.setAttribute("disabled", true);
+
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -11,7 +15,9 @@ const queryForData = (e) => {
             content.innerHTML = "";
             response.forEach(e => {
                 content.innerHTML += `<p>${response}</p>`
-            });            
+            });
+
+            button.removeAttribute("disabled");      
         }
     };
     xhttp.open("GET", `/api/generate/paragraphs?count=${num.value}&dictionary=${dict.value}`, true);
